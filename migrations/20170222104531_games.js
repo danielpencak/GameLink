928df7d6824f0535c7e97bc6f8c5492222b79dc7
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-02-22 10:50:40 -0800

    Add games table migration

---
+++ migrations/20170222104531_games.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('games', table => {
+    table.increments();
+    table.string('name').notNullable().unique();
+    table.text('description');
+    table.string('image_url');
+    table.string('type');
+    table.integer('min_players').notNullable();
+    table.integer('max_players').notNullable();
+    table.timestamps(true, true);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('games');
+};

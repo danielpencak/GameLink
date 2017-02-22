eb2ce14591d3ed1e231b39773df9fe0a7da75251
Daniel Pencak <danielpencak@gmail.com>
2017-02-22 10:55:41 -0800

    Create players migration

---
+++ migrations/20170222104634_players.js
@@ -0,0 +1,19 @@
+'use strict'
+
+exports.up = function(knex) {
+  return knex.schema.createTable('players', table => {
+    table.increments();
+    table.string("username").unique().notNullable();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.string("email").unique().notNullable();
+    table.integer("skill_level").notNullable();
+    table.string("avatar")
+    table.text("bio");
+    table.string("birth_date");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('players')
+};

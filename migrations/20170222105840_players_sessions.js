be0a1792ff467c17d78388824f141ce106e76291
Daniel Pencak <danielpencak@gmail.com>
2017-02-22 11:27:13 -0800

    Create games seed

--- migrations/20170222105840_players_sessions.js
+++ migrations/20170222105840_players_sessions.js
@@ -3,8 +3,16 @@
 exports.up = function(knex) {
   return knex.schema.createTable("players_sessions", table => {
     table.increments();
-    table.integer("player_id").index().references("players.id").onDelete("cascade").notNullable();
-    table.integer("session_id").index().references("sessions.id").onDelete("cascade").notNullable();
+    table.integer("player_id")
+      .index()
+      .references("players.id")
+      .onDelete("cascade")
+      .notNullable();
+    table.integer("session_id")
+      .index()
+      .references("sessions.id")
+      .onDelete("cascade")
+      .notNullable();
   })
 };
 
---
+++ seeds/1_games.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-extra-parens*/
+'use strict'
+
+exports.seed = ((knex) => {
+  return knex('games').del()
+    .then(() => {
+      return knex('games').insert([{
+        id: 1,
+        name: 'Monopoly',
+        description: 'Let\'s conquer the real estate market',
+        image_url: 'http://www.fillmurray.com/200/300',
+        type: 'board',
+        min_players: 2,
+        max_players: 4
+      }, {
+        id: 2,
+        name: 'Scrabble',
+        description: 'Let\'s make some words',
+        image_url: 'http://www.fillmurray.com/150/250',
+        type: 'board',
+        min_players: 2,
+        max_players: 4
+      }, {
+        id: 3,
+        name: 'Sorry!',
+        description: 'I am not sorry!',
+        image_url: 'http://www.fillmurray.com/100/200',
+        type: 'board',
+        min_players: 2,
+        max_players: 4
+      }]);
+    })
+    .then(() => {
+     return knex.raw(
+       "SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));"
+     );
+   });
+});

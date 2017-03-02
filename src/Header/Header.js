6ebce26fcf1ed02bab01db565760c6b977818e7f
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-02 10:08:16 -0800

    Improve css of login buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 import './Header.css';
 import { Link } from 'react-router';
 import PlayerHeader from '../PlayerHeader/PlayerHeader';
 
 export default function Header(props) {
   return (
-    <Navbar className="Header" collapseOnSelect>
+    <nav className="Header">
       <Link to={props.userId ? '/dashboard' : '/'}>GameLink</Link>
-      <div className="account">
          {
           props.userId
-          ? <PlayerHeader username={props.username} handleLogout={props.handleLogout} />
-          : <Nav pullRight>
-              <NavItem eventKey={1} href="#">
-                <Button name='loginModalOpen' onClick={props.toggleModal} bsStyle='primary'>Login</Button>
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                <Button name='signupModalOpen' onClick={props.toggleModal} bsStyle='primary'>Signup</Button>
-              </NavItem>
-            </Nav>
+          ? <div className="account">
+              <PlayerHeader username={props.username} handleLogout={props.handleLogout} />
+            </div>
+          : <div className="loginButtons">
+              <button name='loginModalOpen' onClick={props.toggleModal}>Login</button>
+              <button name='signupModalOpen' onClick={props.toggleModal}>Signup</button>
+            </div>
         }
-      </div>
-    </Navbar>
+    </nav>
   );
 }

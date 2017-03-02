28b1d771559326fa4e739685159a5a1f1776e662
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-02 09:20:56 -0800

    Improve header css

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,27 +6,22 @@ import PlayerHeader from '../PlayerHeader/PlayerHeader';
 
 export default function Header(props) {
   return (
-    <Navbar className='Navbar' inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Link to={props.userId ? '/dashboard' : '/'}>GameHub</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
+    <Navbar className="Header" collapseOnSelect>
+      <Link to={props.userId ? '/dashboard' : '/'}>GameLink</Link>
+      <div className="account">
          {
-          props.userId ? <PlayerHeader username={props.username} handleLogout={props.handleLogout} />
-          :
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">
-              <Button name='loginModalOpen' onClick={props.toggleModal} bsStyle='primary'>Login</Button>
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              <Button name='signupModalOpen' onClick={props.toggleModal} bsStyle='primary'>Signup</Button>
-            </NavItem>
-          </Nav>
+          props.userId
+          ? <PlayerHeader username={props.username} handleLogout={props.handleLogout} />
+          : <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                <Button name='loginModalOpen' onClick={props.toggleModal} bsStyle='primary'>Login</Button>
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                <Button name='signupModalOpen' onClick={props.toggleModal} bsStyle='primary'>Signup</Button>
+              </NavItem>
+            </Nav>
         }
-      </Navbar.Collapse>
+      </div>
     </Navbar>
   );
 }
--- src/PlayerHeader/PlayerHeader.js
+++ src/PlayerHeader/PlayerHeader.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './PlayerHeader.css';
-import { NavDropdown, MenuItem } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap'
 
-export default function PlayerHeader(props) {
-  return (
+class PlayerHeader extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { menuIsOpen: false }
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({ menuIsOpen: !this.state.menuIsOpen })
+  }
+  render() {
+    const { props, state, toggleMenu } = this;
+    return (
       <div className="PlayerHeader">
-        <img src={`https://robohash.org/${props.username}`} alt={`${props.username}'s Avatar`}/>
-        <NavDropdown title={props.username} id="player-menu-dropdown">
-          <MenuItem onClick={props.handleLogout}>Logout</MenuItem>
-        </NavDropdown>
+        <div className="playerIcon" onClick={ toggleMenu }>
+          <Col className="username" smHidden xsHidden>
+            {props.username}
+          </Col>
+          <img src={`https://robohash.org/${props.username}`} alt={`${props.username}'s Avatar`}/>
+        </div>
+        {
+          state.menuIsOpen
+          ? <Button onClick={props.handleLogout} className="logOut" bsStyle="primary">Logout</Button>
+          : null
+        }
       </div>
-  );
+    );
+  }
 }
+
+export default PlayerHeader;
